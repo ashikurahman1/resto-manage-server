058e8a5a384d2ef4b393c0584df4a695439203ec
ashikurahman1 <ashikurahmantuhin@gmail.com>
2026-01-06 00:55:07 +0600

    API: Create new order

---
+++ controllers/orderController.js
@@ -0,0 +1,49 @@
+import db from '../db/mysql.js';
+
+// Create a new order
+export const createOrder = async (req, res) => {
+  try {
+    const user_id = req.user.id;
+
+    const { items } = req.body;
+
+    if (!items || items.length === 0) {
+      return res.status(400).json({ message: 'Order items are required' });
+    }
+
+    // calculate total price
+    let total_price = 0;
+    for (const item of items) {
+      const [food] = await db.execute('SELECT price FROM foods WHERE id = ?', [
+        item.food_id,
+      ]);
+      if (food.length === 0) {
+        return res
+          .status(400)
+          .json({ message: `Food item with id ${item.food_id} not found` });
+      }
+      total_price += food[0].price * item.quantity;
+    }
+
+    const [orderResult] = await db.execute(
+      'INSERT INTO orders (user_id, total_price, status) VALUES (?, ?, ?)',
+      [user_id, total_price]
+    );
+    const order_id = orderResult.insertId;
+
+    for (let item of items) {
+      const [food] = await db.execute('SELECT price FROM foods WHERE id = ?', [
+        item.food_id,
+      ]);
+      await db.execute(
+        'INSERT INTO order_items (order_id, food_id, quantity, price) VALUES (?, ?, ?, ?)',
+        [order_id, item.food_id, item.quantity, food[0].price]
+      );
+    }
+    res.status(201).json({ message: 'Order created successfully', order_id });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: 'Failed to create order', error: error.message });
+  }
+};
